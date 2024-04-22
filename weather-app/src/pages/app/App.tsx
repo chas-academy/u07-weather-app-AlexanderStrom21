0c330c44cdb83f2689622986509223b7713f2a73
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-04-22 16:09:40 +0100

    geolocation

---
+++ weather-app/src/pages/app/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h2>Weather App</h2>
+        <p>Welcome</p>
+      </div>
+    </>
+  )
+}
+
+export default App
